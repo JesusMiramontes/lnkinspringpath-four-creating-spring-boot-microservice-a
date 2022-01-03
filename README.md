82514838e100a160cea42711afff5f2db84daf92
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-01-03 15:01:21 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 4) Linkedin Spring Learning Path: Creating your first Spring Boot Microservice - A
+## Summary
+Code part of course: https://www.linkedin.com/learning/creating-your-first-spring-boot-microservice/
+
+
+## Topics
+
+
+## Tools
---
+++ src/test/java/com/miramontes/four/linkin/becomespringdev/a/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miramontes.four.linkin.becomespringdev.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
